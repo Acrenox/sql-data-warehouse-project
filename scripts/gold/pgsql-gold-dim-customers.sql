b5d501fe00831647e5c030a8f9cd84d387fe44b0
swapnila95 <44964840+swapnila95@users.noreply.github.com>
2025-11-04 18:58:37 +0530

    Create gold.dim_customers view for data warehouse
    This script creates views for the Gold layer in the data warehouse, combining data from the Silver layer to produce a clean, enriched dataset for analytics and reporting.

--- scripts/gold/pgsql-gold-dim-customers.sql
+++ scripts/gold/pgsql-gold-dim-customers.sql
@@ -1,3 +1,24 @@
+/*
+===============================================================================
+DDL Script: Create Gold Views
+===============================================================================
+Script Purpose:
+    This script creates views for the Gold layer in the data warehouse. 
+    The Gold layer represents the final dimension and fact tables (Star Schema)
+
+    Each view performs transformations and combines data from the Silver layer 
+    to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+    - These views can be queried directly for analytics and reporting.
+===============================================================================
+*/
+
+-- =============================================================================
+-- Create Dimension: gold.dim_customers
+-- =============================================================================
+
+
 --dim_customer_table
 CREATE OR REPLACE VIEW gold.dim_customers AS 
 SELECT 
@@ -56,4 +77,5 @@ ON ci.cst_key=ca.cid
 LEFT JOIN silver.erp_loc_a101 la
 ON ci.cst_key=la.cid
 ORDER BY 1, 2;
+
 
