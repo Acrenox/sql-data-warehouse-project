114827703a3cd7c74dde7a45cf3ebebbe17b7c3e
swapnila95 <44964840+swapnila95@users.noreply.github.com>
2025-11-04 13:20:49 +0530

    Refactor SQL transformations and validation checks

--- scripts/silver/pgsql_silver_transformation_checks_crm_sales_details.sql
+++ scripts/silver/pgsql_silver_transformation_checks_crm_sales_details.sql
@@ -3,51 +3,46 @@
 --UPDATED silver.crm_sales_details WITH THE TRANSFORMATIONS (Not to be run without the Pipeline):---
 ------------------------------------------------------------------------------------------------
 INSERT INTO silver.crm_sales_details(
-        sls_ord_num,
-        sls_prd_key,
-        sls_cust_id,
-        sls_order_dt,
-        sls_ship_dt,
-        sls_due_dt,
-        sls_sales,
-        sls_quantity,
-        sls_price
-    )
-    SELECT
-        sls_ord_num,
-        sls_prd_key,
-        sls_cust_id,
-        CASE
-            WHEN sls_order_dt IS NULL THEN NULL
-            WHEN sls_order_dt::numeric <= 0 OR char_length(sls_order_dt::text) <> 8 THEN NULL
-            ELSE to_date(sls_order_dt::text, 'YYYYMMDD')
-        END AS sls_order_dt,
-        CASE
-            WHEN sls_ship_dt IS NULL THEN NULL
-            WHEN sls_ship_dt::numeric <= 0 OR char_length(sls_ship_dt::text) <> 8 THEN NULL
-            ELSE to_date(sls_ship_dt::text, 'YYYYMMDD')
-        END AS sls_ship_dt,
-        CASE
-            WHEN sls_due_dt IS NULL THEN NULL
-            WHEN sls_due_dt::numeric <= 0 OR char_length(sls_due_dt::text) <> 8 THEN NULL
-            ELSE to_date(sls_due_dt::text, 'YYYYMMDD')
-        END AS sls_due_dt,
-        -- sls_sales
-        CASE
-            WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales <> sls_quantity * ABS(sls_price)
-                THEN sls_quantity * sls_price
-            ELSE sls_sales
-        END AS sls_sales,
-        -- sls_quantity
-        sls_quantity,
-        -- sls_price
-        CASE
-            WHEN sls_price IS NULL OR sls_price <= 0
-                THEN (sls_sales / NULLIF(sls_quantity, 0))
-            ELSE ABS(sls_price)
-        END AS sls_price
-    FROM bronze.crm_sales_details;
-
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+sls_order_dt,
+sls_ship_dt,
+sls_due_dt,
+sls_sales,
+sls_quantity,
+sls_price
+)
+SELECT
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+CASE WHEN sls_order_dt<=0 OR LENGTH(sls_order_dt::text)!=8 THEN NULL
+		ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+END sls_order_dt,
+CASE WHEN sls_ship_dt<=0 OR LENGTH(sls_ship_dt::text)!=8 THEN NULL
+		ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+END sls_ship_dt,
+CASE WHEN sls_due_dt<=0 OR LENGTH(sls_due_dt::text)!=8 THEN NULL
+		ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+END sls_due_dt,
+--Rules
+/* 	1. If sls_sales is negative, 0, Null, then derive it using Quantity and Price.
+	2. If sls_price is zero or null, calculate it using sls_sales and sls_quantity.
+	3. If price is negative, convert it to a positive value. */
+--sls_sales
+CASE WHEN sls_sales IS NULL OR sls_sales<=0 OR sls_sales!=sls_quantity*ABS(sls_price) 
+		THEN sls_quantity*sls_price
+	ELSE sls_sales
+END sls_sales,
+--sls_quantity
+sls_quantity,
+---sls_price
+CASE WHEN sls_price IS NULL OR sls_price<=0
+		THEN sls_sales/NULLIF(sls_quantity, 0)
+	ELSE ABS(sls_price)
+END sls_price
+FROM bronze.crm_sales_details;
 ------------------------------------------------------------------------------------------------
 --CHECKS STARTS HERE--
 ------------------------------------------------------------------------------------------------
@@ -111,4 +106,5 @@ WHERE sls_cust_id NOT IN (
     SELECT DISTINCT cst_id FROM silver.crm_cust_info
 );
 --🟢 Purpose: Ensures referential and logical consistency across the data model.
+
 
