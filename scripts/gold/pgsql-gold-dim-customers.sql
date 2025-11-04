31810c59682a2ba25ff9f8cd59d1291c15099be2
swapnila95 <44964840+swapnila95@users.noreply.github.com>
2025-11-04 18:57:06 +0530

    Created fact and dimension tables from silver

---
+++ scripts/gold/pgsql-gold-dim-customers.sql
@@ -0,0 +1,59 @@
+--dim_customer_table
+CREATE OR REPLACE VIEW gold.dim_customers AS 
+SELECT 
+	ROW_NUMBER() OVER (ORDER BY ci.cst_create_date, ci.cst_key) AS customer_key,
+	ci.cst_id AS customer_id,
+	ci.cst_key AS customer_number,
+	ci.cst_firstname AS first_name,
+	ci.cst_lastname AS last_name,
+	la.cntry AS country,
+	ci.cst_marital_status AS marital_status,
+	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr --CRM is the Master for gender info
+		ELSE COALESCE(ca.gen, 'n/a')
+	END AS gender,
+	ci.cst_create_date AS create_date,
+	ca.bdate AS birthdate
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+ON ci.cst_key=ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+ON ci.cst_key=la.cid
+WHERE ci.cst_id > 0
+ORDER BY cst_id;
+
+
+--Check Duplicates
+SELECT cst_id, COUNT(*) FROM
+(SELECT 
+	ci.cst_id,
+	ci.cst_key,
+	ci.cst_firstname,
+	ci.cst_lastname,
+	ci.cst_marital_status,
+	ci.cst_gndr,
+	ci.cst_create_date,
+	ca.bdate,
+	ca.gen,
+	la.cntry
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+ON ci.cst_key=ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+ON ci.cst_key=la.cid)t
+GROUP BY cst_id
+HAVING COUNT(*)>1;
+
+--Match the gender columns from all the tables
+SELECT DISTINCT
+	ci.cst_gndr,
+	ca.gen,
+	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr --CRM is the Master for gender info
+		ELSE COALESCE(ca.gen, 'n/a')
+	END AS new_gen
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+ON ci.cst_key=ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+ON ci.cst_key=la.cid
+ORDER BY 1, 2;
+
